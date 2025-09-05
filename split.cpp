1d050783eca5554f9bdab33ea99c2b299a261c1c
Chenyang Wang <cwang120@usc.edu>
2025-09-05 01:08:17 +0000

    all done with part 1

--- split.cpp
+++ split.cpp
@@ -11,6 +11,7 @@ the function below should be the only one in this file.
 */
 
 #include "split.h"
+#include <cstddef> //solving the "NULL is not defined in this scope" error as instructed by the guide
 
 /* Add a prototype for a helper function here if you need */
 
@@ -18,6 +19,23 @@ void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
 // WRITE YOUR CODE HERE
+  if (in == NULL){
+    return;
+  }
+  else{
+    if ((in->value)%2 == 0){
+      evens=in; 
+      in=in->next;
+      evens->next=NULL;
+      split(in,odds,evens->next);
+    }
+    else{
+      odds=in;
+      in=in->next;
+      odds->next=NULL;
+      split(in,odds->next,evens);
+    }
+  }
 }
 
 /* If you needed a helper function, write it here */
